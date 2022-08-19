4beaadc9406990d48cb647b86859fe9adb66bac8
paul bratslavsky <paulbratslavsky@pauls-MacBook-Pro-2.local>
2022-08-19 12:58:14 -0500

    fixed footer and social icon

--- app/api/events/get-event-by-slug.server.js
+++ app/api/events/get-event-by-slug.server.js
@@ -1,5 +1,5 @@
 export async function getEventBySlug(slug) {
-  const baseUrl = "https://squid-app-7h5w9.ondigitalocean.app" || process.env.BASE_URL
+  const baseUrl = process.env.BASE_URL
   const query = `/api/event/find-by-slug/${slug}?populate=*`
   return await fetch(baseUrl + query)
 }
